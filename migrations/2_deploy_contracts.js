05d2edc9c93c39cc74467c94b2614ac14385cd39
Ahmdfdhilah <ahmad.121140173@student.itera.ac.id>
2024-06-17 15:03:47 +0700

    adding development deploy for smartcontracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 const CarInsurance = artifacts.require("CarInsurance");
 
-module.exports = function (deployer) {
-  deployer.deploy(CarInsurance);
+module.exports = function(deployer) {
+  deployer.deploy(CarInsurance, { gas: 6000000 }); 
 };
--- truffle-config.js
+++ truffle-config.js
@@ -6,22 +6,38 @@ const deployerPrivateKey = process.env.DEPLOYER_PRIVATE_KEY;
 
 module.exports = {
   networks: {
-    polygon: {
+    development: {
+      host: "127.0.0.1",    
+      port: 8545,       
+      network_id: "*",   
+      gas: 8000000,         
+      gasPrice: 2000000000,  
+    },
+    polygon_mumbai: { 
       provider: () => new HDWalletProvider(
         deployerPrivateKey,
-        `https://polygon-mainnet.infura.io/v3/${infuraProjectId}`,
-        0,
-        10, 
-        true, 
-        "m/44'/60'/0'/0/", 
+        `wss://polygon-mumbai.infura.io/ws/v3/${infuraProjectId}`,
       ),
-      network_id: 137, 
-      gas: 5500000,
+      network_id: 80001, 
+      gas: 8000000, 
+      gasPrice: 1000000000, 
+      confirmations: 2,
+      timeoutBlocks: 200,
+      networkTimeout: 10000000,
+    },
+    polygon: { //prod
+      provider: () => new HDWalletProvider(
+        deployerPrivateKey,
+        `wss://polygon-mainnet.infura.io/ws/v3/${infuraProjectId}`,
+      ),
+      network_id: 137,
+      gas: 6000000, 
+      gasPrice: 15000000000,
       confirmations: 2,
       timeoutBlocks: 200,
       skipDryRun: true,
       networkTimeout: 10000,
-      pollingInterval: 15000 
+      pollingInterval: 15000
     },
   },
   compilers: {
